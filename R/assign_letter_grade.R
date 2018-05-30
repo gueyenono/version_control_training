1c4e81e1335b7b65dc93a20a9e48c9995b5e9bd4
Ghislain Nono Gueye <nonoghislain@gmail.com>
2018-05-30 10:38:54 -0500

    The use of the if/else control structure did not allow for vectorized operations. Used the cut() function instead, which also conveniently outputs factors.

--- R/assign_letter_grade.R
+++ R/assign_letter_grade.R
@@ -1,11 +1,9 @@
 assign_letter_grade <- function(x){
 	
-	if(x < 60) letter_grade <- "F"
-	else if(x >= 60 & x < 70) letter_grade <- "D"
-	else if(x >= 70 & x < 80) letter_grade <- "C"
-	else if(x >= 80 & x < 90) letter_grade <- "B"
-	else if(x >= 90) letter_grade <- "A"
-	
-	letter_grade
+	cut(x = x,
+		 breaks = c(-Inf, 60, 70, 80, 90, Inf),
+		 labels = c("F", "D", "C", "B", "A"),
+		 include.lowest = TRUE,
+		 right = FALSE)
 }
 
