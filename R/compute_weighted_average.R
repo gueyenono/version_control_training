97a0151ff977b8185079bf1678e1fb1ec542766f
Ghislain Nono Gueye <nonoghislain@gmail.com>
2018-05-30 17:53:51 -0500

    Made the function more efficient with less code.

--- R/compute_weighted_average.R
+++ R/compute_weighted_average.R
@@ -1,13 +1,4 @@
-library()
-
 compute_weighted_average <- function(x, weight){
-	n <- ncol(x)
-	sum <- x[,1]
-	
-	for(i in 2:n){
-		sum <- sum + x[,i]
-	}
-	
-	sum/n * weight
+	rowMeans(x) * weight
 }
 
